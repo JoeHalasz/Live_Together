4932e6ab827549f18ef26e648a9ab5ed154d265d
JoeHalasz <jhalasz@yahoo.com>
2021-10-17 21:54:00 -0400

    Debug printing

--- mainHost.py
+++ mainHost.py
@@ -3,7 +3,7 @@ from client import *
 from host import host
 from game import game
 from world import *
-
+import time
 
 player, world = loadWorld() # load the world
 # connect with server 
@@ -14,14 +14,30 @@ my_actions = []
 send_world(s[0], world)
 
 while True:
+	timebefore = time.perf_counter()
 	send_data(s[0], player, my_actions) # the host sends the first bit of data
+	timeAfterSend = time.perf_counter()
 	my_actions = [] # reset my actions 
 	if (other_player != ""):
 		breaking, my_actions = game(player, other_player, gameTick, world)
 		if breaking:
 			break
-	
+	timeAfterLoop = time.perf_counter()
 	other_player, other_actions = recieve_data(s[0], player, world)
+	timeAfterRecieve = time.perf_counter()
 	dealWithActions(other_actions, player, world)
 	gameTick += 1
+	timeAfterActions = time.perf_counter()
+
+	timeForSend = timeAfterSend - timebefore
+	timeForLoop = timeAfterLoop - timeAfterSend
+	timeForRecieve = timeAfterRecieve - timeAfterLoop
+	timeForActions = timeAfterActions - timeAfterRecieve
+	print()
+	print("Time for send: " + str(timeForSend))
+	print("Time for Loop: " + str(timeForLoop))
+	print("Time for recv: " + str(timeForRecieve))
+	print("Time for acts: " + str(timeForActions))
+
+
 	
