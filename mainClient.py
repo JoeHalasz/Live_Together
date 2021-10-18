5fcf606eba4d924625b838a8fc8ef0b214f50bf9
JoeHalasz <jhalasz@yahoo.com>
2021-10-17 23:14:45 -0400

    threads!

--- mainClient.py
+++ mainClient.py
@@ -2,6 +2,9 @@
 from client import *
 from game import game
 from world import *
+import threadStuff
+import threading
+
 
 # connect with server 
 s = connect()
@@ -10,15 +13,25 @@ player, world = recieve_world(s) # this will download the world from the other p
 gameTick = 0
 my_actions = []
 
+threadStuff.create(player, world)
+t = threading.Thread(target=threadStuff.clientThread, args=(s,))
+t.start()
 
-while True:
-	other_player, other_actions = recieve_data(s, player, world) # the client recieves the first bit of data
-	dealWithActions(other_actions, player, world)
-	if (other_player != ""):
-		breaking, my_actions = game(player, other_player, gameTick, world)
-		if breaking:
-			break
-	send_data(s, player, my_actions)
-	my_actions = [] # reset my actions 
-	gameTick += 1
-	
+try: # need this so that the other thread stops if there is an error
+	while True:
+		if len(other_actions) != 0:
+			dealWithActions(other_actions, player, world)
+			other_actions = []
+		if (other_player != ""):
+			breaking, my_actions = game(player, other_player, gameTick, world)
+			if breaking:
+				break
+		globalPlayer = player
+		globalWorld = world
+		gameTick += 1
+except:
+	pass
+
+global stop
+stop = True
+print("here")
--- mainHost.py
+++ mainHost.py
@@ -3,25 +3,36 @@ from client import *
 from host import host
 from game import game
 from world import *
+import threadStuff
+import threading
+
 
 player, world = loadWorld() # load the world
 # connect with server 
 s = host()
-other_player = ""
 gameTick = 0
-my_actions = []
 send_world(s[0], world)
 
-while True:
-	send_data(s[0], player, my_actions) # the host sends the first bit of data
-	my_actions = [] # reset my actions 
-	if (other_player != ""):
-		breaking, my_actions = game(player, other_player, gameTick, world)
-		if breaking:
-			break
-	other_player, other_actions = recieve_data(s[0], player, world)
-	dealWithActions(other_actions, player, world)
-	gameTick += 1
+threadStuff.create(player, world)
+t = threading.Thread(target=threadStuff.hostThread, args=(s[0],))
+t.start()
 
+try: # need this so that the other thread stops if there is an error
+	while True:
+		if (other_player != ""):
+			breaking, my_actions = game(player, other_player, gameTick, world)
+			if breaking:
+				break
+		
+		if len(other_actions) != 0:
+			dealWithActions(other_actions, player, world)
+			other_actions = []
+		globalPlayer = player
+		globalWorld = world
+		gameTick += 1
+except:
+	pass
 
-	
+global stop
+stop = True
+print("here")
